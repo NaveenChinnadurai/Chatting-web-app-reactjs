2c66d0c47f28c1c601d84573b5f0ed4d2d172386
Naveen Chinnadurai <111662328+NaveenChinnadurai@users.noreply.github.com>
2023-08-06 18:43:14 +0530

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,23 @@ import Signin from './pages/signin'
 import OnBoarding from './pages/onboarding'
 import Chatscreen from './pages/chatscreen'
 import ChatPage from './pages/chatpage'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Top from './components/top'
 function App() {
 
   return (
     <div className="app-div">
-      <Top/>
+      <Top />
       {/* routing to different pages of the app using browerRouter component */}
-      <Router>
-        <Routes>
-          <Route path='/' Component={Splash}/>
-          <Route path='/onboarding' Component={OnBoarding}/>
-          <Route path='/signin' Component={Signin}/>
-          <Route path='/signup' Component={Signup}/>
-          <Route path='/splash' Component={Splash}/>
-          <Route path='/chatpage' Component={Chatscreen}/>
-          <Route path='/mainchat' Component={ChatPage}/>
-        </Routes>
-    </Router>
+      <Routes>
+        <Route path='/' Component={Splash} />
+        <Route path='/onboarding' Component={OnBoarding} />
+        <Route path='/signin' Component={Signin} />
+        <Route path='/signup' Component={Signup} />
+        <Route path='/splash' Component={Splash} />
+        <Route path='/chatpage' Component={Chatscreen} />
+        <Route path='/mainchat' Component={ChatPage} />
+      </Routes>
     </div>
   )
 }
