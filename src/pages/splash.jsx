443cecb0d8c220fc9961f98e59d8695d2f8cb805
dev.Naveen <111662328+NaveenChinnadurai@users.noreply.github.com>
2023-08-16 09:25:00 +0000

    3 sec intro page

--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState,useEffect } from 'react'
 import '../styles/pageStyles/splash.css'
 import logo from '../assets/logo.png'
 import OnBoarding from './onboarding'
 function Splash() {
-  /* Intro page of the chatting app */
-  return (
-    <Link to='/onboarding' className='splash-div'>
-          <img src={logo} alt="logo-img" className='splash-logo-img' />
-          <span className='splash-txt'>Back to Vedas 🕉️</span>
-    </Link>
+  const IntroPage=()=>{
+    return(
+      /* Intro page of the chatting app */
+      <div className='splash-div'>
+            <img src={logo} alt="logo-img" className='splash-logo-img' />
+            <span className='splash-txt'>Back to Vedas 🕉️</span>
+      </div>
   )
+  }
+  const [intro,setIntro]=useState(<IntroPage/>);
+  const [isIntro, setIsIntro] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIntro(<OnBoarding />);
+      setIsIntro(false);
+    }, 3000);
+  }, []);
+
+  if (isIntro) {
+    return <IntroPage/>;
+  }
+  return(
+    <div>{intro}</div>
+  )
+  
 }
 
 export default Splash
